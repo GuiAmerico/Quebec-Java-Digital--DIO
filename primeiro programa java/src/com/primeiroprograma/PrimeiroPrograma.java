93eae0b87c8c82ded10581bfb7729836ef32b602
GuiAmerico <guilhermao127@gmail.com>
2022-09-15 09:23:08 -0300

    adicionando alterações

--- primeiro programa java/src/com/primeiroprograma/PrimeiroPrograma.java
+++ primeiro programa java/src/com/primeiroprograma/PrimeiroPrograma.java
@@ -1,11 +1,22 @@
 package com.primeiroprograma;
 
+import com.primeiroprograma.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		int a = 2;
-		int b = 3;
-		System.out.println("Hello world"+ (a+b));
+//		int a = 2;
+//		int b = 3;
+//		System.out.println("Hello world"+ (a+b));
+
+		Gato gato01 = new Gato();
+		Gato gato02 = new Gato();
+		Livros livro = new Livros();
 	}
 
+}
+
+class Livros {
+	private String nome;
+	private String npag;
 }
--- primeiro programa java/src/com/primeiroprograma/model/Gato.java
+++ primeiro programa java/src/com/primeiroprograma/model/Gato.java
@@ -1,5 +1,67 @@
 package com.primeiroprograma.model;
 
+import java.util.Objects;
+
 public class Gato {
+	private String nome;
+	private String gato;
+	private Integer idade;
+
+	public Gato() {
+
+	}
+
+	public Gato(String nome, String gato, Integer idade) {
+		this.nome = nome;
+		this.gato = gato;
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getGato() {
+		return gato;
+	}
+
+	public void setGato(String gato) {
+		this.gato = gato;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(gato, idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gato other = (Gato) obj;
+		return Objects.equals(gato, other.gato) && Objects.equals(idade, other.idade)
+				&& Objects.equals(nome, other.nome);
+	}
+
+	@Override
+	public String toString() {
+		return "Gato [nome=" + nome + ", gato=" + gato + ", idade=" + idade + "]";
+	}
 
 }
